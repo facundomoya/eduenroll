9ef3266f5b05cf44bf8b1bb46c88d6d0f762ab05
facundo moya <facundomoya18@hotmail.com>
2025-04-28 11:55:14 -0300

    code review: added VerifyToken function to all user routes.

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -4,11 +4,11 @@ import { VerifyToken } from "../../middleware/verifyToken.js";
 
 const route = express.Router();
 
-route.get("/user", VerifyToken,userController.getAllUsers);
+route.get("/user", VerifyToken, userController.getAllUsers);
 route.get("/user/:id", VerifyToken, userController.getUser);
-route.post("/user/administrator", userController.addAdminUser);
-route.post("/user/professor", userController.addProfessorUser);
-route.delete("/user/:id", userController.deleteProfessorUser);
-route.put("/user/:id", userController.updateUser);
+route.post("/user/administrator", VerifyToken, userController.addAdminUser);
+route.post("/user/professor", VerifyToken, userController.addProfessorUser);
+route.delete("/user/:id", VerifyToken, userController.deleteProfessorUser);
+route.put("/user/:id", VerifyToken, userController.updateUser);
 
 export default route;
